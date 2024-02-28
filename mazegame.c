e4d2916a65f92b229834a84660f67087d07074e7
mkirk05 <146748614+mkirk05@users.noreply.github.com>
2024-02-28 23:19:44 +0000

    maze game skellington code

--- mazegame.c
+++ mazegame.c
@@ -4,7 +4,33 @@
 //struct for the maze
 
 //function for loading in the maze 
+    //load in the file
+    //check for it being empty - Error: Empty maze
+    //check parameters - Error: Maze does not meet requirements
+    //check the layout of the maze - Error: Invalid maze
+
+//function for exchanging the s in the text file to an X to mark the users position
+
+//function to test if the X has reached the E should be done after each turn
+    //if not then allow the user to carry on with the game 
+    // if yes then display the winning message 
+
+//function to check if the spot that the user wants to move is a wall
+    // if not update user position 
+    // if yes do not update position and display message - You have hit a wall
 
 int main(){
+
+    //take in the users input 
+    //what to do with the users input
+
+    //may want to be in a while loop so that the user can keep inputting in order to move
+    // input = m then print file 
+    // input = w then update the users position to 1 up 
+    // input = s then update the users position to 1 down 
+    // input = a then update the users position to 1 right 
+    // input = d then update the users position to 1 left
+
+    
 
 }
